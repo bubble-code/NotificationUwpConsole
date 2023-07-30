95b3a8ae28453659183a653b17498111557a307d
a.obregon <a.obregon@HOME-WORK>
2023-07-30 03:07:55 -0700

    Add project files.

---
+++ notif/Command.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommandLine;
+
+namespace notif
+{
+    class MiCommand
+    {
+        [Option('c', "head", Required = false, HelpText = "Texto de cabecera")]
+        public string? Head { get; set; } = " ";
+        [Option('b', "body", Required = false, HelpText = "Texto del cuerpo")]
+        public string? Body { get; set; } = " ";
+    }
+}
---
+++ notif/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Windows.UI.Notifications;
+using CommandLine;
+using notif;
+using Windows.UI.Notifications;
+using Microsoft.Toolkit.Uwp.Notifications;
+
+namespace Noti
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Parser.Default.ParseArguments<MiCommand>(args).WithParsed<MiCommand>(opt =>
+            {
+                RunProgram(opt);
+            }).WithNotParsed(errors => HandleParsingErrors(errors));        }
+
+        static void RunProgram(MiCommand options)
+        {
+            /* var xml= ToastNotificationManager.GetTemplateContent(ToastTemplateType.ToastText01);
+              //var text = xml.GetElementsByTagName("text");
+              var text = xml.GetElementsByTagName("text");
+              text[0].AppendChild(xml.CreateTextNode(options.Body));
+              var toast = new ToastNotification(xml);
+
+              ToastNotificationManager.CreateToastNotifier(options.Head).Show(toast);*/
+
+
+
+            new ToastContentBuilder()
+     .AddArgument("action", "viewConversation")
+     .AddArgument("conversationId", 9813)
+     .AddText(options.Head)
+     .AddText(options.Body)
+     .Show();
+        }
+
+        static void HandleParsingErrors(IEnumerable<Error> errors)
+        {
+            Console.WriteLine("Errors: " +  errors);    
+        }
+    }
+}
